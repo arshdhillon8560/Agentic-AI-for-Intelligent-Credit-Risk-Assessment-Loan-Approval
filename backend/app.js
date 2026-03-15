5961e51114ac5fa92eb89a1eb3a36559f8758d25
arshdhillon8560 <arshdhillon8560@gmail.com>
2026-03-16 02:46:38 +0530

    App.js correction

--- backend/app.js
+++ backend/app.js
@@ -6,28 +6,25 @@ const cors = require("cors")
 const authRoutes = require("./routes/authRoutes")
 const kycRoutes = require("./routes/kycRoutes")
 const applicationRoutes = require("./routes/applicationRoutes")
+const officerRoutes = require("./routes/officerRoutes")
 
 const app = express()
 
-// Middleware
 app.use(cors())
 app.use(express.json())
 
-
 // Routes
-app.use("/auth", authRoutes)
-app.use("/kyc", kycRoutes)
-app.use("/application", applicationRoutes)
+app.use("/auth",authRoutes)
+app.use("/kyc",kycRoutes)
+app.use("/application",applicationRoutes)
+app.use("/officer",officerRoutes)
 
-
-// Test route
-app.get("/", (req,res)=>{
-    res.send("Loan Approval Backend Running")
+app.get("/",(req,res)=>{
+ res.send("Loan Approval Backend Running")
 })
-
 
 const PORT = 5000
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT,()=>{
+ console.log(`Server running on port ${PORT}`)
 })
